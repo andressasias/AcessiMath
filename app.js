49713020320a90b4182a77608bb8482f50578fb5
Andressa Sias <andressa.sias@cwi.com.br>
2021-06-25 17:31:31 -0300

    Em andamento

--- app.js
+++ app.js
@@ -15,13 +15,17 @@ app.get('/atividades', (req, res) => {
     res.sendFile(path.resolve('src/html/atividades.html'));
 })
 
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.resolve('src/html/sobre.html'));
+})
+
 app.get('/api/atividades/:nivel', async(req, res) => {
     const { params: { nivel } } = req
     const row = await db.selectAtividadeByLevel(nivel)
     res.json(row);
     // res.json({ id })
 })
-
+//process.env.PORT
 app.listen(3000, () => {
     console.log('Ok')
 })
--- src/js/script-atividades.js
+++ src/js/script-atividades.js
@@ -43,23 +43,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
             confirmButtonText: 'Continuar aprendendo'
           }).then((result) => {
             if (result.isConfirmed) {
+              //salvando última questão nos cookies, caso o user volte para a home
+              localStorage.setItem('lastQuestionIndex', this.currentQuestionIndex + 1); 
+              localStorage.setItem('lastLevel', this.questions[this.currentQuestionIndex + 1].nivel); 
               this.currentQuestionIndex = this.currentQuestionIndex + 1;
               this.currentQuestion = this.questions[this.currentQuestionIndex]
+
+              if (!this.currentQuestion) {
+                Swal.fire({
+                  title: 'Parabéns!',
+                  imageUrl: 'https://i.pinimg.com/originals/b8/4a/3e/b84a3eb595247aae0a6095943d24edf4.gif',
+                  imageWidth: 150,
+                  text: 'Você respondeu todas as perguntas dessa atividade!',
+                  confirmButtonText: 'Fazer outra atividade',
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                    window.location.href = '/';
+                  }
+                })
+              }
             }
           })
-          if (!this.currentQuestion) {
-            Swal.fire({
-              title: 'Parabéns!',
-              imageUrl: 'https://i.pinimg.com/originals/b8/4a/3e/b84a3eb595247aae0a6095943d24edf4.gif',
-              imageWidth: 150,
-              text: 'Você respondeu todas as perguntas dessa atividade!',
-              confirmButtonText: 'Fazer outra atividade',
-            }).then((result) => {
-              if (result.isConfirmed) {
-                window.location.href = '/';
-              }
-            })
-          }
         } else {
           Swal.fire({
             title: 'Não foi dessa vez...',
@@ -75,6 +79,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
             if (result.isConfirmed) {
 
             }else{
+              //salvando última questão nos cookies, caso o user volte para a home
+              localStorage.setItem('lastQuestionIndex', this.currentQuestionIndex + 1); 
+              localStorage.setItem('lastLevel', this.questions[this.currentQuestionIndex + 1].nivel); 
+
               this.questions.push(this.questions[this.currentQuestionIndex])
               this.currentQuestionIndex = this.currentQuestionIndex + 1;
               this.currentQuestion = this.questions[this.currentQuestionIndex]
@@ -90,14 +98,38 @@ window.addEventListener('DOMContentLoaded', (event) => {
           .then(res => res.json())
           .then(data => {
 
+            /*
             this.questions = data.map(x => ({
               ...x,
               questions: getQuestions(x)
-            }))
-            
-            this.currentQuestion = this.questions[0]
-            this.level = id;
+            }))*/
 
+            console.log("data")
+            console.log(data)
+            console.log(data.length)
+
+            for(var i = localStorage.getItem('lastQuestionIndex') - 1; i <  data.length; i++){
+              console.log("comecei")
+              console.log(data[0])
+            }
+
+            this.questions = data.map(x => {
+              console.log(x);
+              return ({
+                        ...x,
+                        questions: getQuestions(x)
+                    })
+            })
+            
+            if(localStorage.getItem('lastQuestionIndex') && localStorage.getItem('lastLevel') == id){
+              
+              console.log("já tava fazendo antes")
+              this.currentQuestion = this.questions[localStorage.getItem('lastQuestionIndex')]
+              this.level = localStorage.getItem('lastLevel');
+            }else{
+              this.currentQuestion = this.questions[0]
+              this.level = id;
+            }
           })
           .catch(err => {
             console.log('err', err)
